e67169d8608f8dabaaa09e9fbe6b17e7849d76f9
Alperen <bitirgenalperen@gmail.com>
2021-02-08 17:33:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
     //Camera Authentication
-    var ip_address = "192.168.1.111"
+    var ip_address = "192.168.1.111" //ip adress of the camera
     //camera username and password
     var username = "admin";
     var password = "admin";
@@ -12,32 +12,17 @@
         wsPort: 9999    
     });
 
-
-    var HOST = '10.16.1.30'
-    var PORT = 8080;
-   
-    var server = net.createServer();
-    server.listen(PORT, HOST);
-    const socket = require("./controllers/socket")(server);
-    app.get("/socket/connection", socket.connection);
-
-    /*
-
     var net = require('net');
-    let setval = 417;
+    let setval = 417; //just some number
 
-
+    var ip_listen = "10.16.1.30"; //ip of the port listening
+    var port_listen = "8080";
     var buf = new Buffer(1024);
     buf.writeInt32LE(setval, 0); //max value to send 2147483647
 
     let client = new net.Socket();
-    client.connect("8080", "10.16.1.30", () => {
-    console.log("Connected");
-    buf.writeInt32LE(setval);
-    client.write(buf, () => {})
+    client.connect(port_listen, ip_listen , () => {
+        console.log("Connected");
+        buf.writeInt32LE(setval);
+        client.write(buf, () => {})
     });
-
-    */
-
-
-
